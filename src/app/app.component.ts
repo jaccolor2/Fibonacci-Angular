cb2382a86256ab9fb468e2be237fb16cb089983b
jaccolor2 <98039916+jaccolor2@users.noreply.github.com>
2023-05-07 18:46:59 +0200

    hey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fibonacci';
+
+  fibo: undefined;
+
+  ngOnInit() {
+    //this.fibo = ;
+  }
+
+  private getNthFib(n: number) {
+    const fib = [0, 1];
+    function getFib(n: number) {
+      const nextFib = fib[fib.length - 1] + fib[fib.length - 2];
+      if (n > nextFib) {
+        fib.push(nextFib);
+        getFib(n);
+      }
+    }
+    getFib(n);
+    return fib;
+  }
+
+  getVal(event: Event) {
+    console.log(event);
+
+    this.fibo = this.test(Number((event.target as HTMLInputElement).value));
+    return this.fibo;
+  }
+
+  public test(this: any, n: number) {
+    if (n < 2) return n;
+    return this.test(n - 1) + this.test(n - 2);
+  }
 }
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: `<h1>Fibonacci result : {{ fibo }}</h1>`,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `,
+  ],
+})
+export class HelloComponent {
+  @Input() fibo: string | undefined;
+}
